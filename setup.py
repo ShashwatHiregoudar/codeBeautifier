63b38f1c12208bb14a61cbf907bd681a13bb9eaf
Shashwat Hiregoudar <shashwathiregoudar@gmail.com>
2019-08-15 13:56:59 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='codeBeautifier',
+    version='0.0.0',
+    description='my try on creating a custom python library',
+    packages=['codeBeautifier'],
+    author='Shashwat Hiregoudar',
+    author_email='shashwathiregoudar@gmail.com',
+    keywords=['example'],
+    url='https://github.com/ShashwatHiregoudar/codeBeautifier'
+)
